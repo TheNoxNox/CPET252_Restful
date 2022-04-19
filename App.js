f24b972ebc9c28b80b0b5f62685601c2e4f11783
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2022-04-18 20:03:46 -0400

    edit get

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import logo from './logo.svg';
-
+import { useParams} from 'react-router-dom';
 import React from 'react';
 
 
@@ -10,14 +10,16 @@ import React from 'react';
 function App() {
   
   //const element = <h1>Hello, world</h1>
-
+  const { p } = useParams();
+  
   return (
     <div className="App">
       <h1>Employees</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="other" element={<Other />} />
-        <Route path="edit" element={<Edit />} />
+        <Route path="/edit/:p" element={<Edit />}  />
+        
         <Route path="Add" element={<Add />} />
 
       </Routes>
@@ -51,15 +53,20 @@ function Other()
     </>
   );
 }
-function Edit()
+const Edit = (props)=>
 {
+  console.log("ah");
   return (
     <>
     <nav>
     <Link to="/"> Home </Link>
   </nav>
     <div className="ed">
-      <EditEmployee/>
+      <EditEmployee
+      {...props}
+      params = {useParams()}
+      />
+
     </div>
     </>
   );
@@ -103,11 +110,9 @@ class Current {
   }
 
   componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(), 1000
-    );
+    
       let self = this;
-      fetch('http://64.72.1.43/rest/employees', {
+      fetch('http://64.72.1.192/rest/employees', {
           method: 'GET'
       }).then(function(response) {
           if (response.status >= 400) {
@@ -120,10 +125,8 @@ class Current {
       console.log('caught it!',err);
       })
   }
-  tick()
-  {
-   
-  }
+  
+  
   render() {
     
       return (
@@ -146,7 +149,7 @@ class Current {
                       </tr>
                   </thead>
                   <tbody>
-                  {this.state.employees.map(employee =>
+                 {this.state.employees.map(employee =>
                       <tr key={employee.emp_id}>
                       <td>{employee.fname} </td>
                       <td>{employee.minit} </td>
@@ -155,7 +158,7 @@ class Current {
                       <td>{employee.job_lvl} </td>
                       <td>{employee.pub_id} </td>
                       <td>{employee.hire_date} </td>
-                      <td><a>Edit</a></td>
+                      <td><Link to={"/edit/" + employee.emp_id}> Edit </Link></td>
                       </tr>
                   )}
                   </tbody>
@@ -180,44 +183,33 @@ class EditEmployee extends React.Component {
   }
 
   componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(), 1000
-    );
-      let self = this;
+    let self = this;
+    let emp = this.props.params;
+      
       //add employee id to end
-      fetch('http://64.72.1.43/rest/employees', {
+      fetch('http://64.72.1.192/rest/employees/' + emp.p, {
           method: 'GET'
       }).then(function(response) {
           if (response.status >= 400) {
               throw new Error("Bad response from server");
           }
+          
           return response.json();
+          
       }).then(function(data) {
+        console.log("ah2");
           self.setState({employees: data});
+          
       }).catch(err => {
       console.log('caught it!',err);
       })
   }
-  componentDidUnmount() {
-    
-      let self = this;
-      //add employee id to end
-      fetch('http://64.72.1.43/rest/employees', {
-          method: 'PUT'
-      }).then(function(response) {
-          if (response.status >= 400) {
-              throw new Error("Bad response from server");
-          }
-          return response.json();
-      }).then(function(data) {
-        //send stuff here
-          self.setState({employees: data});
-      }).catch(err => {
-      console.log('caught it!',err);
-      })
-  }
-  render() {    
-      return (        
+  
+  render() {  
+      
+    console.log(this.state.employees[0]);   
+      return (     
+        
           <div className="container"> 
           <div className="panel panel-default p50 uth-panel">
               <table className="table table-dark" border="1px solid black">
@@ -234,16 +226,136 @@ class EditEmployee extends React.Component {
                       </tr>
                   </thead>
                   <tbody>
-                  {this.state.employees.map(employee =>
-                      <tr key={employee.emp_id}>
-                      <td>{employee.fname} </td>
-                      <td>{employee.minit} </td>
-                      <td>{employee.lname} </td>
-                      <td><Input value={employee.job_id}> </Input> </td>
-                      <td><Input value={employee.job_lvl}> </Input> </td>
-                      <td>{employee.pub_id} </td>
-                      <td>{employee.hire_date} </td>
-                      <td><a>Edit</a></td>
+                  {this.state.employees.map(({emp_id, fname, minit, lname, job_id, job_lvl, pub_id, hire_date}) =>
+                      <tr >
+                      <td>{fname} </td>
+                      <td>{minit} </td>
+                      <td>{lname} </td>
+                     <td> <input defaultValue={job_id}></input> </td>      count: props.start,
+356
+      start : props.start,
+357
+      max: props.max 
+358
+    };
+359
+​
+360
+    this.j = props.jolly; 
+361
+    
+362
+  }
+363
+​
+364
+  // 3. lifecycle method - called after the component is rendered to the DOM
+365
+  componentDidMount() {
+366
+    this.timerID = setInterval(
+367
+      () => this.tick(), 1000
+368
+    );
+369
+  }
+370
+​
+371
+  // another lifecycle method 
+372
+  componentWillUnmount() {
+373
+    clearInterval(this.timerID); // kill timer 
+374
+  }
+375
+​
+376
+  increment()
+377
+  {
+378
+    if(this.state.count >= this.state.max) return this.state.start; 
+379
+​
+380
+    return this.state.count + 1; 
+381
+  }
+382
+​
+383
+  tick() {
+384
+​
+385
+    // this should be the only place you change state, so render is called.  
+386
+​
+387
+    this.setState({  // 'schedules' call to render 
+388
+      datex: new Date(), // new value for inside of state property
+389
+      count: this.increment()
+390
+    });
+391
+  }
+392
+​
+393
+  // 2. called after ctor by React
+394
+  render() {
+395
+    return (
+396
+      <div>
+397
+        <h1>Hello {this.j}</h1>
+398
+        <h2>It is {this.state.datex.toLocaleTimeString()}.</h2>
+399
+        <h2>Count {this.state.count}</h2>
+400
+      </div>
+401
+    );
+402
+  }
+403
+}
+404
+​
+405
+// has to be after the creation of Clock
+406
+// could pass args here or in the call above.
+407
+// OR you can just  use <Clock, as you see here.  
+408
+// NOTE input for properties.  
+409
+const Clocker = <Clock jolly="ken" start={5} max={10}/>; 
+410
+//const Clocker =<Clock oi="Employees" s
+411
+​
+412
+​
+413
+​
+414
+​
+415
+export default App;
+                     <td> <input defaultValue={job_lvl}></input> </td>
+                      <td>{pub_id} </td>
+                      <td>{hire_date} </td>
+                      
                       </tr>
                   )}
                   </tbody>
@@ -268,7 +380,7 @@ class AddEmployee extends React.Component {
 
   componentDidUnmount() {
       let self = this;
-      fetch('http://64.72.1.43/rest/employees', {
+      fetch('http://64.72.1.192/rest/employees', {
           method: 'POST'
       }).then(function(response) {
           if (response.status >= 400) {
